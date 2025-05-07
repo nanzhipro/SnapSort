6b987f4091bef9a733d67f7384ba42a8e307de60
南知 <deer_hope@live.cn>
2025-05-07 08:32:54 +0800

    feat: SnapSort launch

---
+++ SnapSort/SnapSortApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapSortApp.swift
+//  SnapSort
+//
+//  Created by 南朋友 on 2025/5/7.
+//
+
+import SwiftUI
+
+@main
+struct SnapSortApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
